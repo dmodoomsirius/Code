c3c567f156ad134da829f94cca651d992988fcef
dmodoomsirius <dmodoomsirius@users.noreply.github.com>
2015-07-24 23:02:50 -0400

    make it check the command_prefix and use that.

--- python/Cloudbot/bbm.py
+++ python/Cloudbot/bbm.py
@@ -8,7 +8,7 @@ def bbmStaff(text, message, chan):
             message("Owners: Dmodoomsirius, DarkGuardsman");
             #message("Texture Artist: Morton0000");
             message("Senior Developers: Kolatra")
-            message("Junior Developers: Kolatra, shobu9, TheCowGod, Hennamann")
+            message("Junior Developers: shobu9, TheCowGod, Hennamann")
     else:
             message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
 
@@ -28,7 +28,7 @@ def chans(text, message):
     message("The official BuiltBroken Channels are: " + " , ".join(bbmChannels))
     
 @hook.command("bbmhelp")
-def bbmhelp(text, message):
-    message("If you are looking for who is the staff type .bbmstaff")
-    message ("if you are looking for our sponsors please type .sponsor")
-    message("If you are looking for our official channels please do .bbmchan")
+def bbmhelp(text, message, bot):
+    message("If you are looking for who is the staff type " + bot.config["connections"][0]["command_prefix"] + "bbmstaff")
+    message ("if you are looking for our sponsors please type " + bot.config["connections"][0]["command_prefix"] + "sponsor")
+    message("If you are looking for our official channels please do  " + bot.config["connections"][0]["command_prefix"] + "bbmchan")
