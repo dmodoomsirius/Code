088babdaefc2ffa951c4857bacdd929e15f17630
dmodoomsirius <dmodoomsirius@gmail.com>
2015-09-25 14:13:27 -0400

    update join channels and bbm_packs

--- python/Cloudbot/bbm_packs.py
+++ python/Cloudbot/bbm_packs.py
@@ -1,8 +1,10 @@
 from cloudbot import hook
 
+bbmChannels = ["#bbm-bots","#bbm-dev","#builtbroken","#builtbrokenmodding","#bbm-packs","#icbm","#artillects "]
+
 @hook.command("voltz")
 def bbmVoltz(text, message, chan):
-    if chan in ("#BBM-bots", "#BBM-Dev", "#BBM-Packs", "#BuiltBrokenModding", "#BuiltBroken"):
+    if any(x in chan for x in bbmChannels):
                 message("Voltz for minecraft version 1.7.10 is in beta");
                 message ("we are hoping to have a beta out sometime next month");
                 return
@@ -10,7 +12,7 @@ def bbmVoltz(text, message, chan):
 
 @hook.command("Ampz", "Ampzreloaded")
 def bbmAmpzReloaded(text, message, chan):
-    if chan in ("#BBM-bots", "#BBM-Dev", "#BBM-Packs", "#BuiltBrokenModding", "#BuiltBroken", "#dmodtest"):
+    if any(x in chan for x in bbmChannels):
                 message("Ampz-reloaded is the new imporoved Ampz.")
                 message("It is currently on hold.")
                 return
@@ -18,14 +20,14 @@ def bbmAmpzReloaded(text, message, chan):
 
 @hook.command("omhz")
 def omhz(text, message, chan):
-    if chan in ("#BBM-bots", "#BBM-Dev", "#BBM-Packs", "#BuiltBrokenModding", "#BuiltBroken"):
+    if any(x in chan for x in bbmChannels):
                 message("currently on hold until Voltz is done.")
                 return
     message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
     
 @hook.command("packs")
 def bbmpacks(text, message, chan):
-    if chan in ("#BBM-bots", "#BBM-Dev", "#BBM-Packs", "#BuiltBrokenModding", "#BuiltBroken"):
+    if any(x in chan for x in bbmChannels):
                 message("For Voltz type .voltz")
                 message("For AmpzReloaded info type either .Ampz or .Ampzreloaded")
                 message("For omhz type .omhz")
--- python/Cloudbot/join.py
+++ python/Cloudbot/join.py
@@ -1,4 +1,3 @@
-
 from cloudbot import hook
 
 @hook.irc_raw('004')
@@ -7,5 +6,8 @@ def onReadyJoin(conn, bot):
     conn.send("JOIN #dmodtest")
     conn.send("JOIN #BuiltBrokenModding")
     conn.send("JOIN #BuiltBroken")
-    conn.send("JOIN #Razz")
+    #conn.send("JOIN #Razz")
     conn.send("JOIN #MadScience")
+    con.send("JOIN #icbm')
+    conn.send("JOIN #Kolatra")
+    conn.send("JOIN #Kolatra-Dev")
